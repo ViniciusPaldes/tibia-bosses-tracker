c3ee4d9f7b417a0ba1bfb9984a3906bdf650e3f5
Vinicius Paldês <viniciuspaldes@Viniciuss-MacBook-Pro.local>
2023-06-27 13:49:59 -0300

    Chance calculation

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   saveKillStatisticsToFirestore,
   fetchBossesLastDayKilled,
+  getDaysSinceLastKill,
+  calculateBossChance,
 } from '../services/firebase';
 
 const AdminPage = () => {
@@ -13,12 +15,26 @@ const AdminPage = () => {
     fetchBossesLastDayKilled();
   };
 
+  const handleDaysSinceLastKill = () => {
+    getDaysSinceLastKill();
+  };
+
+  const handleChance = () => {
+    calculateBossChance();
+  }
+
   return (
     <div>
       <h1>Admin Page</h1>
       <button onClick={handleSaveKillStatistics}>Save Kill Statistics</button>
       <button onClick={handleFetchBossesLastDayKilled}>
         Fetch Bosses Last Day Killed
+      </button>
+      <button onClick={handleDaysSinceLastKill}>
+        Get the last kill for a boss
+      </button>
+      <button onClick={handleChance}>
+        Get chance
       </button>
     </div>
   );
