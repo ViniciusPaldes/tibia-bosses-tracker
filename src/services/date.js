a9d713d3daf77ba2e4e2f5c426a447e8b7fc11a2
Vinicius Paldês <viniciuspaldes@Viniciuss-MacBook-Pro.local>
2023-07-19 14:21:58 -0300

    Bug fix when checking bosses

--- src/services/date.js
+++ src/services/date.js
@@ -1,21 +1,20 @@
-
 export const getMostRecentTimestamp = (boss) => {
-    if (boss.checks && boss.checks.length > 0) {
-      const sortedChecks = boss.checks.sort((a, b) => {
-        if (!a.timestamp && !b.timestamp) {
-          return 0;
-        } else if (!a.timestamp) {
-          return 1;
-        } else if (!b.timestamp) {
-          return -1;
-        } else {
-          return b.timestamp.seconds - a.timestamp.seconds;
-        }
-      });
-      const mostRecentCheck = sortedChecks[0];
-      const date = mostRecentCheck.timestamp?.toDate();
-      return date.toLocaleString('pt-BR');
-    } else {
-      return '-';
-    }
-  };
+  if (boss?.checks && boss?.checks.length > 0) {
+    const sortedChecks = boss?.checks.sort((a, b) => {
+      if (!a?.timestamp && !b?.timestamp) {
+        return 0;
+      } else if (!a?.timestamp) {
+        return 1;
+      } else if (!b?.timestamp) {
+        return -1;
+      } else {
+        return (b?.timestamp?.seconds || 0) - (a?.timestamp?.seconds || 0);
+      }
+    });
+    const mostRecentCheck = sortedChecks[0];
+    const date = mostRecentCheck?.timestamp?.toDate();
+    return date?.toLocaleString('pt-BR') || '-';
+  } else {
+    return '-';
+  }
+};
